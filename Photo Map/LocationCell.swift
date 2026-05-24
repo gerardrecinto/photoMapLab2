c6e16b4a58368f9653515291e34fdd8fdf90b022
Gerard Louis Andres Recinto <grecinto@ucsd.edu>
2026-05-24 15:56:02 -0700

    Fix force-unwrap on URL and categories array index in LocationCell

--- Photo Map/LocationCell.swift
+++ Photo Map/LocationCell.swift
@@ -18,14 +18,12 @@ class LocationCell: UITableViewCell {
             nameLabel.text = location["name"] as? String
             addressLabel.text = location.value(forKeyPath: "location.address") as? String
 
-            let categories = location["categories"] as? NSArray
-            if (categories != nil && categories!.count > 0) {
-                let category = categories![0] as! NSDictionary
-                let urlPrefix = category.value(forKeyPath: "icon.prefix") as! String
-                let urlSuffix = category.value(forKeyPath: "icon.suffix") as! String
-
-                let url = "\(urlPrefix)bg_32\(urlSuffix)"
-                categoryImageView.loadImage(from: URL(string: url)!)
+            if let categories = location["categories"] as? NSArray, categories.count > 0,
+               let category = categories[0] as? NSDictionary,
+               let urlPrefix = category.value(forKeyPath: "icon.prefix") as? String,
+               let urlSuffix = category.value(forKeyPath: "icon.suffix") as? String,
+               let url = URL(string: "\(urlPrefix)bg_32\(urlSuffix)") {
+                categoryImageView.loadImage(from: url)
             }
         }
     }
